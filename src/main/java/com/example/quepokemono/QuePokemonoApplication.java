3e6bd50b50f1c352bc6f0102d24de87e61a8393c
Sr.Irvin <SR._IRVIN@HOTMAIL.COM>
2025-04-01 13:03:07 -0600

    Primer commit - Proyecto Spring Boot

---
+++ src/main/java/com/example/quepokemono/QuePokemonoApplication.java
@@ -0,0 +1,13 @@
+package com.example.quepokemono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuePokemonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuePokemonoApplication.class, args);
+    }
+
+}
